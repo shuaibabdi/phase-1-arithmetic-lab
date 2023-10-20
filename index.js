1e15e9a11061107aa1b83eee3b7f58e9e1723109
shuaib abdi <shuaibabdi1015@gmail.com>
2023-10-20 20:02:22 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=31;
+let num2=2;
+const multiply =num1 * num2;
 
-// Write your code here
+
+let random=Math.floor(Math.random() * 10) +1;
+
+let num3= 9;
+let num4= 5;
+const mod= num3 % num4;
+
+const max=Math.max(6,7,8,9,14,16,20);
